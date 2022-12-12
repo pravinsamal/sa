081f34dc4b88dd3636814b4486c486c5c4f35948
pravinsamal <samalpravin0406@gmail.com>
2022-12-13 01:12:08 +0530

    1

---
+++ f1/src/f1/addTwoNumber.java
@@ -0,0 +1,15 @@
+package f1;
+
+public class addTwoNumber {
+	
+	public static void test() {
+		int a=10;
+		int b=5;
+		int c=a+b;
+		System.out.println(c);
+	}
+	public static void main (String []args ) {
+		addTwoNumber.test();
+	}
+
+}
